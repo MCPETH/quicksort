2f85dd422de8adba0d9706fda05f0088bddeb0a0
MCPE TH <thtongame@gmail.com>
2024-04-15 11:48:24 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
